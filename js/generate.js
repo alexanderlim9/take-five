78ee2a094db38af4fc5f20df2b9f7e3162efceaf
Noah Appleby <appleby.n@husky.neu.edu>
2016-12-11 16:04:01 -0500

    separated entries by sense, updated result page

--- js/generate.js
+++ js/generate.js
@@ -11,8 +11,17 @@ function setName() {
 }
 
 if(Cookies.get('first') === undefined) {
-  //setting the array
-  var entries = ['Kara: I taste... MINTY FRESHNESS', 'Alex: I taste... coffee!', 'Noah: I hear... Subway workers talking'];
+  
+  //setting the entry data arrays
+  var sight = ['Kara saw "ARC"', 'Alex saw \"a girl with a periwinkle backpack\"', 'Noah saw \"an excited old man playing the violin!\"', 'Noah saw \"a student presenting and a teacher agressively discussing with him\"', 'Carl saw \"reflections\"'];
+  //@alexlim, please update the rest:
+  var sound = ['Noah: I hear... Subway workers talking', 'Noah: I hear... a subway wrapper crinkling down below', 'Trevor: I hear... subway workers talking', 'Sam: I hear... beep beep beep!'];
+  var taste = ['Kara: I taste... MINTY FRESHNESS', 'Alex: I taste... coffee!', 'Joe: I taste... morning breath', 'Farrah: I taste... coffee in my throat'];
+  var touch = ['Bobo: I feel... chills', 'Farrah: I feel... COLD', 'Noah: I feel... a cool breeze on my skin', 'Alex: I feel... a cold handrail'];
+  var smell = ['Trevor: I smell... subway sandwiches', 'Alan: I smell... a cup of joe', 'Clint: I smell... FOOD!'];
+  
+  var entries = [sight, sound, taste, touch, smell];
+  
   var json_entries = JSON.stringify(entries);
   console.log("reset");
   Cookies.set('entries', json_entries);
--- js/results.js
+++ js/results.js
@@ -6,8 +6,18 @@ var entries = JSON.parse(json_entries);
 var data='';
 
 data+='<div><ul>';
-for (var i=0; i<=entries.length - 1; i++) {
-    data+='<li>'+entries[i]+'</li>';
+
+//All entries:
+//for (var i=0; i<=entries.length - 1; i++) {
+//5 random entries:
+//for (var i=0; i<=5; i++) {
+//  data+='<li>'+entries[Math.floor(Math.random() * entries.length)]+'</li>';
+//}
+
+//1 Entry from each sense
+for (var i=0; i<=4; i++) {
+  var len = entries[i].length;
+  data+='<li>'+entries[i][Math.floor(Math.random() * (len))]+'</li>';
 }
 data+='</ul></div>'
 
